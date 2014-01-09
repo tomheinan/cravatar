6194a8045b2bec67176332aae44db1d1e41041c9
Tom Heinan <theinan@onemedical.com>
2014-01-08 21:15:55 -0800

    use mkdirp for cache directory generation

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var mkdirp = require('mkdirp');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send("hi");
+app.get(/^\/([A-Za-z0-9\_]+)/, function(req, res) {
+  res.setHeader('Content-type', "image/png");
+
+
 });
 
-app.listen(3000);
+mkdirp('./cache', function(err) {
+  if (err) {
+    console.log(err)
+  } else {
+    app.listen(3000);
+  }
+});
