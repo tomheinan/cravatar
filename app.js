4b02d0b8baaf7ba71d2fae429fd8e639917a1b9f
Tom Heinan <theinan@onemedical.com>
2014-01-09 17:27:28 -0800

    add gm as dependency

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mkdirp = require('mkdirp');
 var fs = require('fs');
+var gm = require('gm');
 var app = express();
 
 app.get(/^\/([A-Za-z0-9\_]+)/, function(req, res) {
@@ -8,6 +9,8 @@ app.get(/^\/([A-Za-z0-9\_]+)/, function(req, res) {
   var def = fs.readFileSync('./default.png');
   res.end(def, 'binary');
 });
+
+
 
 mkdirp('./cache', function(err) {
   if (err) {
